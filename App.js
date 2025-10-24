20dba96423bb4d213b4e8768ff3630aa24e5b886
Louise Lizé <louise.lize@icloud.com>
2025-10-24 17:19:10 +0200

    feat: add application layout

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-import { Text } from "react-native";
-
+import { ImageBackground, Text, View } from "react-native";
+import { styles } from "./App.style";
+import hotBackground from "./assets/hot.png";
 export default function App() {
-  return <Text>Hello, World!</Text>;
+  return (
+    <>
+      <ImageBackground style={styles.container} source={hotBackground}>
+        <View style={styles.workspace}>
+          <View>
+            <Text>Temperature</Text>
+          </View>
+          <View>
+            <Text>Input</Text>
+          </View>
+          <View>
+            <Text>Button</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </>
+  );
 }
---
+++ App.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  workspace: {
+    height: 450,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+});
