d2b9f92c68fec8ff0cc97ffec1b6391fee24d05f
Louise Lizé <louise.lize@icloud.com>
2025-10-27 15:36:14 +0100

    feat: add temperature conversion functions and update display logic

--- App.js
+++ App.js
@@ -5,18 +5,29 @@ import hotBackground from "./assets/hot.png";
 import InputTemperature from "./components/InputTemperature/InputTemperature";
 import TemperatureDisplay from "./components/TemperatureDisplay/TemperatureDisplay";
 import { DEFAULT_TEMPERATURE, DEFAULT_UNIT } from "./constant";
-
+import {
+  convertTemperatureTo,
+  getOppositeUnit,
+} from "./services/temperature-service";
 export default function App() {
   const [inputTemperatureValue, setInputTemperatureValue] =
     useState(DEFAULT_TEMPERATURE);
   const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+  const oppositeUnit = getOppositeUnit(currentUnit);
+
+  function getConvertedTemperature() {
+    const valueAsFloat = Number.parseFloat(inputTemperatureValue);
+    return Number.isNaN(valueAsFloat)
+      ? ""
+      : convertTemperatureTo(valueAsFloat, oppositeUnit).toFixed(1);
+  }
   return (
     <>
       <ImageBackground style={styles.container} source={hotBackground}>
         <View style={styles.workspace}>
           <TemperatureDisplay
-            value={inputTemperatureValue}
-            unit={currentUnit}
+            value={getConvertedTemperature()}
+            unit={oppositeUnit}
           />
           <InputTemperature
             onChangeText={setInputTemperatureValue}
---
+++ services/temperature-service.js
@@ -0,0 +1,17 @@
+import { UNITS } from "../constant";
+
+export function getOppositeUnit(unit) {
+  if (unit === UNITS.CELSIUS) {
+    return UNITS.FAHRENHEIT;
+  } else {
+    return UNITS.CELSIUS;
+  }
+}
+
+export function convertTemperatureTo(value, unit) {
+  if (unit === UNITS.CELSIUS) {
+    return (value - 32) / 1.8;
+  } else {
+    return value * 1.8 + 32;
+  }
+}
